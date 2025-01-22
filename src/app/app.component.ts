9b11b1438bb48073d61c968d3ee7276ac698eec4
Margarita Golubeva <margaret.golubeva@gmail.com>
2025-01-23 01:37:40 +0300

    feat: implement theme switch service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { TuiRoot } from '@taiga-ui/core';
 
+import { HeaderComponent } from './core/components/header/header.component';
+import { ThemeSwitchService } from './core/services/theme-switch/theme-switch.service';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TuiRoot],
+  imports: [RouterOutlet, TuiRoot, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   public readonly title = 'RSSence-frontend';
+
+  public themeSwitchService = inject(ThemeSwitchService);
 }
