1a1b6c2e534f3a55b82772126e64b6b8d9ed4d94
Max <133934232+Kleostro@users.noreply.github.com>
2025-02-11 01:51:40 +0300

    feat: update navigation with profile pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 
+import { authGuard, loginGuard } from '@/app/auth/guards/login.guard';
 import { APP_PATH } from '@/app/core/services/navigation/routes';
 
 export const routes: Routes = [
@@ -7,22 +8,45 @@ export const routes: Routes = [
     path: APP_PATH.DEFAULT,
     loadComponent: () => import('./home/pages/home/home.component').then((c) => c.HomeComponent),
     title: 'RSS | Home',
+    canActivate: [authGuard],
   },
   {
     path: APP_PATH.LOGIN.toLowerCase(),
     loadComponent: () => import('./auth/pages/login/login.component').then((c) => c.LoginComponent),
     title: `RSS | ${APP_PATH.LOGIN}`,
+    canActivate: [loginGuard],
   },
   {
     path: APP_PATH.SIGN_UP.toLowerCase(),
     loadComponent: () =>
       import('./auth/pages/registration/registration.component').then((c) => c.RegistrationComponent),
     title: `RSS | ${APP_PATH.SIGN_UP}`,
+    canActivate: [loginGuard],
   },
   {
     path: APP_PATH.PROFILE.toLowerCase(),
     loadComponent: () => import('./profile/pages/profile/profile.component').then((c) => c.ProfileComponent),
     title: `RSS | ${APP_PATH.PROFILE}`,
+    canActivate: [authGuard],
+    children: [
+      {
+        path: APP_PATH.DEFAULT,
+        redirectTo: APP_PATH.ME.toLowerCase(),
+        pathMatch: 'full',
+      },
+      {
+        path: APP_PATH.ME.toLowerCase(),
+        loadComponent: () =>
+          import('./profile/pages/profile-me/profile-me.component').then((c) => c.ProfileMeComponent),
+        title: `RSSence | ${APP_PATH.PROFILE} | ${APP_PATH.ME}`,
+      },
+      {
+        path: APP_PATH.SETTINGS.toLowerCase(),
+        loadComponent: () =>
+          import('./profile/pages/profile-settings/profile-settings.component').then((c) => c.ProfileSettingsComponent),
+        title: `RSSence | ${APP_PATH.PROFILE} | ${APP_PATH.SETTINGS}`,
+      },
+    ],
   },
   {
     path: APP_PATH.NOT_FOUND,
--- src/app/core/services/navigation/routes.ts
+++ src/app/core/services/navigation/routes.ts
@@ -3,6 +3,8 @@ export const APP_PATH = {
   LOGIN: 'Login',
   SIGN_UP: 'Sign-up',
   PROFILE: 'Profile',
+  SETTINGS: 'Settings',
+  ME: 'Me',
   NOT_FOUND: '404',
   NO_MATCH: '**',
 } as const;
@@ -11,6 +13,8 @@ export const APP_ROUTE = {
   LOGIN: `/${APP_PATH.LOGIN.toLowerCase()}`,
   SIGN_UP: `/${APP_PATH.SIGN_UP.toLowerCase()}`,
   PROFILE: `/${APP_PATH.PROFILE.toLowerCase()}`,
+  PROFILE_ME: `/${APP_PATH.PROFILE.toLowerCase()}/${APP_PATH.ME.toLowerCase()}`,
+  PROFILE_SETTINGS: `/${APP_PATH.PROFILE.toLowerCase()}/${APP_PATH.SETTINGS.toLowerCase()}`,
   NOT_FOUND: `/${APP_PATH.NOT_FOUND.toLowerCase()}`,
   HOME: '/',
 } as const;
