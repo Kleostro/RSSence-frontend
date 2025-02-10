426870fc86449f0ff43bee56d45209184a593ccf
Max <133934232+Kleostro@users.noreply.github.com>
2025-02-11 01:56:44 +0300

    fix: profile button in sidebar

--- src/app/core/components/sidebar/sidebar.component.ts
+++ src/app/core/components/sidebar/sidebar.component.ts
@@ -1,5 +1,4 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { RouterLink } from '@angular/router';
 
 import { MenuItem } from 'primeng/api';
 import { BadgeModule } from 'primeng/badge';
@@ -9,12 +8,13 @@ import { Ripple } from 'primeng/ripple';
 import { TieredMenu } from 'primeng/tieredmenu';
 
 import { AuthService } from '@/app/auth/services/auth/auth.service';
+import { NavigationService } from '@/app/core/services/navigation/navigation.service';
 import { APP_ROUTE } from '@/app/core/services/navigation/routes';
 import { ThemeSwitchService } from '@/app/core/services/theme-switch/theme-switch.service';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [DrawerModule, ButtonModule, TieredMenu, BadgeModule, Ripple, RouterLink],
+  imports: [DrawerModule, ButtonModule, TieredMenu, BadgeModule, Ripple],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -22,10 +22,11 @@ import { ThemeSwitchService } from '@/app/core/services/theme-switch/theme-switc
 export class SidebarComponent {
   private readonly authService = inject(AuthService);
   public readonly themeSwitchService = inject(ThemeSwitchService);
+  public readonly navigationService = inject(NavigationService);
+
   public readonly APP_ROUTE = APP_ROUTE;
 
-  public IsVisible = false;
-
+  public isVisible = false;
   public items: MenuItem[] = [];
 
   public logout(): void {
