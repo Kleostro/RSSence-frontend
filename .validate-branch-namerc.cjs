bb51697556ecce2a96144e9f6db8c5befed32783
Margarita Golubeva <margaret.golubeva@gmail.com>
2025-05-02 22:55:50 +0300

    ci: update branch validation pattern

--- .validate-branch-namerc.cjs
+++ .validate-branch-namerc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   errorMsg: 'Please use correct branch name',
-  pattern: /^(ci|chore|docs|feat|fix|perf|refactor|style|test)\/RS-0[1-9]-\d{2}\/[a-z_]+$/,
+  pattern: /^(ci|chore|docs|feat|fix|perf|refactor|style|test)\/RS-0[1-9]-\d{2}\/[a-z-]+$/,
 };
 
 // Branch Name Examples:
