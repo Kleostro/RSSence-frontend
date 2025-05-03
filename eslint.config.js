be340f0b7a839c3e4e8d36a4f6601bcddc8258de
Margarita Golubeva <margaret.golubeva@gmail.com>
2025-05-03 16:04:15 +0300

    chore: update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,169 +1,43 @@
+// @ts-check
 import angular from 'angular-eslint';
-import eslintPluginNoRelativeImportPaths from 'eslint-plugin-no-relative-import-paths';
-import unusedImports from 'eslint-plugin-unused-imports';
-import path from 'path';
-import tsEslint from 'typescript-eslint';
-import { fileURLToPath } from 'url';
+import tseslint from 'typescript-eslint';
 
-import { FlatCompat } from '@eslint/eslintrc';
+import eslint from '@eslint/js';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  resolvePluginsRelativeTo: __dirname,
-});
-
-const customRules = {
-  '@typescript-eslint/consistent-type-assertions': ['error', { assertionStyle: 'never' }],
-  '@typescript-eslint/explicit-function-return-type': 'error',
-  '@typescript-eslint/explicit-member-accessibility': [
-    'error',
-    {
-      accessibility: 'explicit',
-      overrides: {
-        constructors: 'off',
-      },
-    },
-  ],
-  '@typescript-eslint/explicit-module-boundary-types': 'error',
-  '@typescript-eslint/method-signature-style': 'error',
-  '@typescript-eslint/no-explicit-any': 'error',
-  '@typescript-eslint/no-floating-promises': 'off',
-  '@typescript-eslint/no-inferrable-types': 'error',
-  '@typescript-eslint/no-unnecessary-type-parameters': 'off',
-  '@typescript-eslint/no-unused-vars': [
-    'error',
-    {
-      args: 'all',
-      argsIgnorePattern: '^_',
-      caughtErrors: 'all',
-      caughtErrorsIgnorePattern: '^_',
-      destructuredArrayIgnorePattern: '^_',
-      varsIgnorePattern: '^_',
-      ignoreRestSiblings: true,
-    },
-  ],
-  '@typescript-eslint/no-use-before-define': 'error',
-  '@typescript-eslint/prefer-for-of': 'off',
-  '@typescript-eslint/unbound-method': ['error', { ignoreStatic: true }],
-  'class-methods-use-this': 'off',
-  curly: ['error', 'all'],
-  'dot-notation': 'off',
-  'implicit-arrow-linebreak': 'off',
-  'function-paren-newline': 'off',
-  'operator-linebreak': 'off',
-  'import/extensions': [
-    'error',
-    'ignorePackages',
-    {
-      js: 'never',
-      jsx: 'never',
-      ts: 'never',
-      tsx: 'never',
-    },
-  ],
-  'import/prefer-default-export': 'off',
-  'lines-between-class-members': [
-    'error',
-    {
-      enforce: [
-        { blankLine: 'always', next: 'method', prev: '*' },
-        { blankLine: 'always', next: '*', prev: 'method' },
-      ],
-    },
-  ],
-  'max-len': ['error', { code: 120, ignoreComments: true }],
-  'max-lines-per-function': ['error', { max: 40, skipBlankLines: true, skipComments: true }],
-  'no-empty-function': 'error',
-  'no-relative-import-paths/no-relative-import-paths': [
-    'error',
-    { allowSameFolder: false, rootDir: 'src', prefix: '@' },
-  ],
-  'no-unused-vars': 'off',
-  'object-curly-newline': 'off',
-  'unused-imports/no-unused-imports': 'error',
-};
-
-export default [
+export default tseslint.config(
   {
-    ignores: [
-      'eslint.config.js',
-      '**/.angular/**',
-      '**/coverage/**',
-      '**/dist/**',
-      '.validate-branch-namerc.cjs',
-      'jest.config.ts',
-      'setup-jest.ts',
+    files: ['**/*.ts'],
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...tseslint.configs.stylistic,
+      ...angular.configs.tsRecommended,
     ],
-  },
-  ...compat.extends('airbnb-base'),
-  ...tsEslint.configs.strictTypeChecked,
-  ...tsEslint.configs.stylisticTypeChecked,
-  ...tsEslint.configs.stylistic,
-  {
-    languageOptions: {
-      ecmaVersion: 'latest',
-      globals: {
-        es6: true,
-      },
-      parserOptions: {
-        project: true,
-        projectService: {
-          defaultProject: './tsconfig.json',
-        },
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
-    plugins: {
-      'unused-imports': unusedImports,
-      'no-relative-import-paths': eslintPluginNoRelativeImportPaths,
-    },
-    rules: {
-      ...customRules,
-    },
-    settings: {
-      'import/resolver': {
-        alias: {
-          extensions: ['.ts', '.js', '.jsx', '.json'],
-          map: [['@', './src']],
-        },
-      },
-    },
-  },
-
-  // Angular configuration
-  {
-    name: 'angular',
-    extends: [...angular.configs.tsRecommended],
-    files: ['*.ts'],
     processor: angular.processInlineTemplates,
     rules: {
-      '@angular-eslint/component-selector': [
-        'error',
-        {
-          prefix: 'app',
-          style: 'kebab-case',
-          type: 'element',
-        },
-      ],
       '@angular-eslint/directive-selector': [
         'error',
         {
+          type: 'attribute',
           prefix: 'app',
           style: 'camelCase',
-          type: 'attribute',
         },
       ],
-      '@angular-eslint/prefer-on-push-component-change-detection': 'error',
+      '@angular-eslint/component-selector': [
+        'error',
+        {
+          type: 'element',
+          prefix: 'app',
+          style: 'kebab-case',
+        },
+      ],
     },
   },
-
-  // HTML files configuration
   {
-    name: 'html',
+    files: ['**/*.html'],
     extends: [...angular.configs.templateRecommended, ...angular.configs.templateAccessibility],
-    files: ['*.html'],
+    rules: {
+      '@angular-eslint/template/prefer-self-closing-tags': ['error'],
+    },
   },
-];
+);
