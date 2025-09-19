dbb43839e09cfd6ae5453c3f9697c2c682958a13
Max <133934232+Kleostro@users.noreply.github.com>
2025-09-19 18:48:39 +0300

    feat: implement post views

---
+++ src/app/post/components/post-views/post-views.component.ts
@@ -0,0 +1,18 @@
+import { ChangeDetectionStrategy, Component, inject, input } from '@angular/core';
+
+import { TooltipModule } from 'primeng/tooltip';
+
+import { PostResponse } from '@/app/api/schemas/posts-response';
+import { NavigationService } from '@/app/core/services/navigation/navigation.service';
+
+@Component({
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [TooltipModule],
+  selector: 'app-post-views',
+  styleUrl: './post-views.component.scss',
+  templateUrl: './post-views.component.html',
+})
+export class PostViewsComponent {
+  public readonly navigationService = inject(NavigationService);
+  public post = input<null | PostResponse>(null);
+}
